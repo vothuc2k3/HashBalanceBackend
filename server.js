2f0bde3a3f8178a4be37d1d687a56093bafac057
vothuc2k3 <vothuc2k3@gmail.com>
2024-08-01 09:11:16 +0700

    letssee

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const { RtcTokenBuilder, RtcRole } = require('agora-access-token');
 const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 
 // INIT EXPRESS
 const app = express();
@@ -13,22 +15,13 @@ const APP_ID = process.env.AGORA_APP_ID;
 const APP_CERTIFICATE = process.env.AGORA_APP_CERTIFICATE;
 
 // CONFIG FIREBASE ADMIN SDK
-const serviceAccount = {
-  type: process.env.TYPE,
-  project_id: process.env.PROJECT_ID,
-  private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY.replace(/\\n/g, '\n') : null,
-  client_email: process.env.CLIENT_EMAIL,
-  client_id: process.env.CLIENT_ID,
-  auth_uri: process.env.AUTH_URI,
-  token_uri: process.env.TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-};
+const serviceAccountPath = path.resolve(__dirname, 'firebase_admin_sdk.json');
 
-if (!serviceAccount.private_key) {
-  throw new Error('The PRIVATE_KEY environment variable is not set or is invalid.');
+if (!fs.existsSync(serviceAccountPath)) {
+  throw new Error(`The service account key file does not exist at path: ${serviceAccountPath}`);
 }
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
