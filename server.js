1a614b5bf2e1dc1cebbf707dbbda167a8d435151
vothuc2k3 <114125898+vothuc2k3@users.noreply.github.com>
2024-11-28 19:55:46 +0700

    ok

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const db = admin.firestore();
 
 // MIDDLEWARE
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 // FUNCTIONS
 
